c73db3202bd4fd86f05518dccf89e261dfb8950a
sourcecodeguy1 <sandovaljulio2@yahoo.com>
2018-12-21 22:59:19 -0800

    modify db.js file

--- db.js
+++ db.js
@@ -17,7 +17,7 @@ let mysql_connection = mysql.createPool({
     waitForConnections : true,
     queueLimit :0,
     host: 'johnny.heliohost.org',
-    user: 'julioweb_root',
+    user: 'julioweb',
     password: 'julio123',
     database: 'julioweb_social_network_nodejs',
     debug    :  true,

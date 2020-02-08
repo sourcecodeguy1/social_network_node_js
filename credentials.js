a03799cc62245faa365d78889944def73d26c187
sourcecodeguy1 <sandovaljulio2@yahoo.com>
2020-02-08 00:21:56 -0800

    Delete credentials.js

--- credentials.js
+++
@@ -1,9 +0,0 @@
-let Credentials = function Cred() {
-
-    const host = "mail.juliowebmaster.com";
-    const user = "postmaster@ddrguy2.juliowebmaster.com";
-    const pass = "shishio1";
-
-};
-
-module.exports = Credentials;

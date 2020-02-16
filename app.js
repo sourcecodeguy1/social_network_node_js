74e436eab972fbffa6335642f6a646aabdab3a3e
sourcecodeguy1 <sandovaljulio2@yahoo.com>
2020-02-16 02:21:00 -0800

    modify file

--- app.js
+++ app.js
@@ -659,7 +659,22 @@ app.post('/forgotpass', function (req, res) {
 
                                 if(rows.length === 1){
 
-                                    res.send("Please check your email for instructions on how to reset your password");
+                                    for(let i = 0; i < rows.length; i++){
+
+                                        let db_expired_token = rows[i].expiration;
+
+                                        if(Date.now() < db_expired_token){
+
+                                            res.send("Please check your email for instructions on how to reset your password");
+
+                                        } else {
+
+                                            let delete_query = "DELETE FROM forgot_pass_tbl WHERE email = ?";
+                                            mysql_connection.query(delete_query, [email], );
+
+                                        }
+
+                                    }
 
 
                                 } else {
